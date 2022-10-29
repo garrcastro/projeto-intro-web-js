284662b08bb16b656535bd54e596f2e0f5a73337
garrcastro <garrcastro@gmail.com>
2022-10-29 01:48:40 -0300

    Criação dos arquivos Index.html, script.js e style.css; Documentação inicial do arquivo html; criação dos arrays iniciais no js.

---
+++ script.js
@@ -0,0 +1,4 @@
+//Arrays gerais iniciais
+const meusCursos = ["HTML e CSS", "Javascript", "APIs REST"];
+const minhasTurmas = ["Shavarath", "Lammania", "Khyber", "Mabar", "Dolurh", "Eberron"];
+const alunos = ["Gabriel Castro", "Vincent Rastelli", "Maiana Andrade"];
